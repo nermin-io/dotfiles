af6e9d193f94d632d5b763fb3b46f14819bd4e16
Nermin Sehic <nermin@rendr.delivery>
2025-05-31 16:52:24 +1000

    Add python LSP and formatter

--- home/.config/nvim/lua/custom/plugins/conform.lua
+++ home/.config/nvim/lua/custom/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
       lua = { 'stylua' },
       go = { 'goimports' },
       yaml = { 'yamlfmt' },
+      python = { 'black' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
