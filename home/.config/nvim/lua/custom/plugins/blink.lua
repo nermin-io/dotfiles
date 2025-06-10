18194c8b0bc66250b38211a4d4cdae6dbb721f9e
Nermin Sehic <nermin@rendr.delivery>
2025-06-11 01:05:49 +1000

    Remove borders

--- home/.config/nvim/lua/custom/plugins/blink.lua
+++ home/.config/nvim/lua/custom/plugins/blink.lua
@@ -67,18 +67,11 @@ return { -- Autocompletion
     },
 
     completion = {
-      menu = {
-        auto_show = true,
-        border = 'rounded',
-      },
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = {
-        auto_show = true,
+        auto_show = false,
         auto_show_delay_ms = 500,
-        window = {
-          border = 'rounded',
-        },
       },
     },
 
