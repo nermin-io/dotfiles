6c6779b7911cfea49fd3944514d7060fcc8229be
Nermin Sehic <nermin@rendr.delivery>
2025-06-24 23:43:57 +1000

    update lualine theme to custom Kanso theme

--- home/.config/nvim/lua/custom/plugins/lualine.lua
+++ home/.config/nvim/lua/custom/plugins/lualine.lua
@@ -2,10 +2,12 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local theme = require 'custom.themes.lualine.kanso'
+
     require('lualine').setup {
       options = {
         icons_enabled = vim.g.have_nerd_font,
-        theme = 'iceberg_dark',
+        theme = theme,
       },
     }
   end,
